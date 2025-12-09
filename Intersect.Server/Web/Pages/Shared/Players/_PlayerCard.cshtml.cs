2892c335f501ab829066f373520ca2677f7cfe85
Kaldar <sean12111@gmail.com>
2025-12-09 08:42:28 -0600

    web

--- Intersect.Server/Web/Pages/Shared/Players/_PlayerCard.cshtml.cs
+++ Intersect.Server/Web/Pages/Shared/Players/_PlayerCard.cshtml.cs
@@ -1,11 +1,10 @@
-using Intersect.Server.Entities;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Intersect.Server.Web.Pages.Shared.Players;
 
 public partial class PlayerCardModel : PageModel
 {
-    public Player? Player { get; set; }
+    public Intersect.Server.Entities.Player? Player { get; set; }
 
     public long? Rank { get; set; }
 
